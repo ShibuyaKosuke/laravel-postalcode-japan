53f21de65972df0fe02a60ed2789235f4c4aa412
ShibuyaKosuke <shibuya@imagenious.co.jp>
2020-03-30 17:06:24 +0900

    mod route

--- src/routes/api.php
+++ src/routes/api.php
@@ -2,7 +2,7 @@
 
 use ShibuyaKosuke\LaravelPostalcodeJapan\App\Http\Controllers\PostalCodeController;
 
-Route::middleware(['api', 'web'])->group(function () {
+Route::middleware(['web'])->group(function () {
     Route::get('ajax/prefectures', PostalCodeController::class . '@prefectures')->name('ajax.prefectures');
     Route::get('ajax/cities/{prefecture}', PostalCodeController::class . '@cities')->name('ajax.cities');
 });
